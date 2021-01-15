3150f78ef37f5320ea71dc2e372a74151e3d0196
Mark Servilla <mark.servilla@gmail.com>
2021-01-14 17:23:22 -0700

    Add initial source

---
+++ src/ip_lookup/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+:Mod: __init__
+
+:Synopsis:
+
+:Author:
+    servilla
+
+:Created:
+    1/14/21
+"""
+
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/ip_lookup/ip_lookup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+:Mod: ip_lookup
+
+:Synopsis:
+
+:Author:
+    servilla
+
+:Created:
+    1/14/21
+"""
+
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    main()
